a9e9ebfe41d5507c0edb3b19b1cad797031851dd
Naim317 <jannatulnayem446@gmail.com>
2024-05-06 15:11:03 +0600

    Added and updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 
                 <div class="mt-16">
                     <div class="">
-                        <a href="https://laravel.com/docs" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <a href="/" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div class="text-center">
                                 <h2 class="mt-6 text-xl text-center font-semibold text-gray-900 dark:text-white">Welcome To Banking System Dashboard</h2>
 
